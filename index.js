dd57beea0a14129307c478397dfa0786bed93c73
AshuG317 <141509715+AshuG317@users.noreply.github.com>
2023-10-15 13:17:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/check",(req, res) => {
-	let num = parseInt(re.body.name);
+	let num = parseInt(req.body.num);
 	let ans = num % 2 == 0 ? "even" : "odd";
 	res.send(ans);
 })
